b670b90fb689b1d39805e4d2ae1caec978cfaa31
maubarrerag <mauricio.barrerag@gmail.com>
2016-10-26 16:23:19 -0500

    Terraform Challenges

--- terraform/challenge_3/main.tf
+++ terraform/challenge_3/main.tf
@@ -1,23 +1,21 @@
-
 provider "google" {
   project = "api-project-939426582926"
   region = "us-central1"
-  credentials = "${file("google.json")}"
+  credentials = "${file("../google.json")}"
 }
 
+resource "google_compute_instance" "als-terraform-assignment3" {
+	name = "als-terraform-assignment3"
+	machine_type = "n1-standard-1"
+  	zone         = "us-central1-a"
+  	disk {
+   	 image = "debian-cloud/debian-8"
+  	}
 
-resource "google_compute_instance" "challenge_1" {
-   name = "test"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-  tags = ["foo", "bar"]
-  disk {
-    image = "debian-cloud/debian-8"
-  }
-  disk {
-    type    = "local-ssd"
-    scratch = true
-  }
+  	disk {
+	    type    = "local-ssd"
+	    scratch = true
+	}
 
   network_interface {
     network = "default"
@@ -32,4 +30,6 @@ resource "google_compute_instance" "challenge_1" {
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
- }
+
+
+} 
